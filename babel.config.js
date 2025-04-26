90586189f3a3fee7f64d37ae827370b613c4e1d2
Erdem Özkök <erdemozkok@hotmail.com.tr>
2025-04-27 00:26:33 +0300

    Enhanced project configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          'src': './src',
+          'pages': './src/pages',
+          '_utils': './src/utils',
+          'elements': './src/elements',
+          'components': './src/components',
+          '@appSettings': './src/appSettings.ts',
+          'pageContainers': './src/pageContainers',
+        },
+      },
+    ],
+  ],
 };
