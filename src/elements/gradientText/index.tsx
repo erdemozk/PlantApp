fd956f3e9ca089cede35da91c39da26dce008887
Erdem Özkök <erdemozkok@hotmail.com.tr>
2025-04-29 00:38:44 +0300

    Refactor GradientText component

--- src/elements/gradientText/index.tsx
+++ src/elements/gradientText/index.tsx
@@ -1,28 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import { Text, StyleProp, TextStyle } from 'react-native';
+import LinearGradient, {
+  LinearGradientProps,
+} from 'react-native-linear-gradient';
 import MaskedView from '@react-native-masked-view/masked-view';
 import style from './style';
 
-const GradientText = props => {
-  return (
-    <MaskedView
-      style={style.container}
-      maskElement={
-        <Text style={[props.style, style.maskedText]}>{props.children}</Text>
-      }>
-      <LinearGradient
-        colors={props.colors || ['black', 'white']}
-        start={props.start || {x: 0, y: 0}}
-        end={props.end || {x: 1, y: 0}}
-        locations={props.locations}
-        useAngle={props.useAngle}
-        angle={props.angle}
-        angleCenter={props.angleCenter}>
-        <Text style={[props.style, style.text]}>{props.children}</Text>
-      </LinearGradient>
-    </MaskedView>
-  );
-};
+type GradientTextProps = LinearGradientProps & {
+  children: React.ReactNode;
+  textStyle?: StyleProp<TextStyle>;
+}
+
+const GradientText: React.FC<GradientTextProps> = ({
+  children,
+  textStyle,
+  colors = ['black', 'white'],
+  start = { x: 0, y: 0 },
+  end = { x: 1, y: 0 },
+  locations,
+  useAngle,
+  angle,
+  angleCenter,
+}) => (
+  <MaskedView
+    style={style.container}
+    maskElement={
+      <Text style={[textStyle, style.maskedText]}>{children}</Text>
+    }
+  >
+    <LinearGradient
+      colors={colors}
+      start={start}
+      end={end}
+      locations={locations}
+      useAngle={useAngle}
+      angle={angle}
+      angleCenter={angleCenter}
+    >
+      <Text style={[textStyle, style.text]}>{children}</Text>
+    </LinearGradient>
+  </MaskedView>
+);
 
 export default GradientText;
